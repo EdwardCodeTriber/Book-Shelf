00bdef1624a8e66183411009b882360ba3c6b96b
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-25 11:56:17 +0200

    Added  function to find books

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-const express = require('express')
-const bodyParse = require('body-parse')
-const port = 3000
+const express = require("express");
+const bodyParse = require("body-parse");
+const port = 3000;
 
-
-app.use(bodyParse.json())
+app.use(bodyParse.json());
 
 // Data storage
 
 let books = [];
 
+// Function to find book
+const findBookByISBN = (isbn) => books.find((book) => book.isbn === isbn);
 
 // Routes
 // GET all books
-app.get('/books', (req, res) =>{
-    res.json(books)
-})
+app.get("/books", (req, res) => {
+  res.json(books);
+});
 
 // POST method to add a new Book
-app.post('/books', (req, res) => {
-    const {title, auther, publisher, publishedDate, isbn} = req.body;
+app.post("/books", (req, res) => {
+  const { title, auther, publisher, publishedDate, isbn } = req.body;
 
-    if (!title || !author || !publisher || !publishedDate || !isbn) {
-        return res.status(400).json({error: 'All fields must be filled'})
+  if (!title || !author || !publisher || !publishedDate || !isbn) {
+    return res.status(400).json({ error: "All fields must be filled" });
+  }
+  const newBook = { title, author, publisher, publishedDate, isbn };
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+
+// PUT/PATCH details of an existing book
+app.put("/books"),
+  (req, res) => {
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
     }
-    const newBook = {title, author, publisher, publishedDate, isbn}
-    books.push(newBook)
-    res.status(201).json(newBook)
-})
 
-// PUT/PATCH details of an existing book 
-app.put('/books'), (req, res) =>{
-    if (!book){
-        return res.status(404).json({error: 'Book not found'})
-    }
-
-    const {title, author, publisher, publishedDate} = req.body;
+    const { title, author, publisher, publishedDate } = req.body;
 
     // Validate fields
     if (title) book.title = title;
@@ -43,9 +45,9 @@ app.put('/books'), (req, res) =>{
     if (publishedDate) book.publishedDate = publishedDate;
 
     res.json(book);
-}
+  };
 
 // DELETE remove book from database
-app.delete('/books', (req, res) =>{
-    res.status(204).send();
-})
+app.delete("/books", (req, res) => {
+  res.status(204).send();
+});

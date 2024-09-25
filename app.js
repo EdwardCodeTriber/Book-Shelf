f32915cbd268e9eea540a9204cf2896ecc2f251b
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-25 10:32:19 +0200

    Added a POST method

--- app.js
+++ app.js
@@ -16,3 +16,14 @@ app.get('/books', (req, res) =>{
     res.json(books)
 })
 
+// POST method to add a new Book
+app.post('/books', (req, res) => {
+    const {title, auther, publisher, publishedDate, isbn} = req.body;
+
+    if (!title || !author || !publisher || !publishedDate || !isbn) {
+        return res.status(400).json({error: 'All fields must be filled'})
+    }
+    const newBook = {title, author, publisher, publishedDate, isbn}
+    books.push(newBook)
+    res.status(201).json(newBook)
+})
